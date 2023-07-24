63b8cd513a812b1be99e9ddbf14748bd06d3ad96
Mikalai Alkhimenka <alkhimenkamikalai90@gmail.com>
2023-07-24 15:58:17 +0200

    Solved

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 150;
+        int b = 120;
+        int d;
+        d = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(d);
     }
 }
